8f83cbbd842e0b6b98b18c2a18f905ad7d5c1bda
EliedersonLinhares <eliederson250@gmail.com>
2021-07-18 13:14:58 -0300

    Implement model,route and controllers for subcategory

---
+++ controllers/sub.js
@@ -0,0 +1,45 @@
+const Sub = require('../models/sub')
+const slugify = require('slugify')
+
+exports.create = async (req, res) => {
+	try {
+		const { name } = req.body
+		const sub = await new Sub({ name, slug: slugify(name) }).save()
+		res.json(sub)
+	} catch (error) {
+		//console.log(error)
+		res.status(400).send('Create subcategory failed')
+	}
+}
+
+exports.list = async (req, res) => {
+	res.json(await Sub.find({}).sort({ createdAt: -1 }).exec())
+}
+
+exports.read = async (req, res) => {
+	let sub = await Sub.findOne({ slug: req.params.slug }).exec()
+	res.json(category)
+}
+
+exports.update = async (req, res) => {
+	const { name } = req.body
+	try {
+		const updated = await Sub.findOneAndUpdate(
+			{ slug: req.params.slug },
+			{ name, slug: slugify(name) },
+			{ new: true }
+		)
+		res.json(updated)
+	} catch (error) {
+		res.status(400).send('Subcategory update failed')
+	}
+}
+
+exports.remove = async (req, res) => {
+	try {
+		const deleted = await Sub.findOneAndDelete({ slug: req.params.slug })
+		res.json(deleted)
+	} catch (error) {
+		res.status(400).send('Subcategory deleted failed')
+	}
+}
