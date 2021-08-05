b1556951659a734a32f095e046351e2b01fe1ff9
EliedersonLinhares <eliederson250@gmail.com>
2021-08-05 12:24:12 -0300

    Delete product function

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,15 @@
 const express = require('express')
-
 const router = express.Router()
 
-//middlewares
+// middlewares
 const { authCheck, adminCheck } = require('../middlewares/auth')
 
-//controller
-const { create, listAll } = require('../controllers/product')
+// controller
+const { create, listAll, remove } = require('../controllers/product')
 
-//routes
+// routes
 router.post('/product', authCheck, adminCheck, create)
-router.get('/products/:count', listAll)
+router.get('/products/:count', listAll) // products/100
+router.delete('/product/:slug', authCheck, adminCheck, remove)
 
 module.exports = router
